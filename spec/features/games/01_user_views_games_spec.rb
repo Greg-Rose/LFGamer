8a8fc629d95f7e2ddb806e8aec32e0c623370b6f
Greg Rose <gregrose628@gmail.com>
2017-06-19 14:11:23 -0400

    wip - added user views games test

---
+++ spec/features/games/01_user_views_games_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'user views games' do
+  # As a user
+  # I want to view a list of games
+  # So that I can see what games I can find people to play with
+  #
+  # Acceptance Criteria:
+  #   - I can view a list of games
+  #   - I can see each game’s title, cover image, and which console(s) it’s on
+  #   - I can click the game to view it’s show page info
+
+  let!(:games) { create_list(:game, 2) }
+
+  scenario 'views list of games' do
+    visit games_path
+
+    games.each do |game|
+      expect(page).to have_content game.name
+    end
+  end
+end
