80fa3c0a2d8a7eaaba75c96df903033171ae43c9
Greg Rose <gregrose628@gmail.com>
2017-06-19 20:13:57 -0400

    added CreateGames migration

---
+++ db/migrate/20170619232602_create_games.rb
@@ -0,0 +1,14 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :name, null: false
+      t.string :cover_photo, null: false
+      t.boolean :online, null: false
+      t.boolean :split_screen, null: false
+
+      t.timestamps
+    end
+
+    add_index :games, :name, unique: true
+  end
+end
