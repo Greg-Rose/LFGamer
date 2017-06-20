a6c57718cce76c2806a087a434190d37ef5447e3
Greg Rose <gregrose628@gmail.com>
2017-06-20 14:52:24 -0400

    added GamesConsole model and associations

--- app/models/console.rb
+++ app/models/console.rb
@@ -1,6 +1,9 @@
 class Console < ApplicationRecord
+  has_many :games_consoles
+  has_many :games, through: :games_consoles
+
   mount_uploader :logo, ConsoleLogoUploader
-  
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :logo, presence: true
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
+  has_many :games_consoles
+  has_many :consoles, through: :games_consoles
+
   mount_uploader :cover_image, GameCoverImageUploader
-  
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :cover_image, presence: true
