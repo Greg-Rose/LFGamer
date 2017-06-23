ba5cd6293646514e9b4ffecb4f0921e04e25a14b
Greg Rose <gregrose628@gmail.com>
2017-06-23 14:13:17 -0400

    added all class method to IGDB classes

--- app/models/igdb.rb
+++ app/models/igdb.rb
@@ -2,29 +2,38 @@ module IGDB
   class Base
     @@headers = { 'X-Mashape-Key' => ENV["IGDB_API_KEY"] }
     @@base_url = "https://igdbcom-internet-game-database-v1.p.mashape.com/"
-    @@defualt_fields = "name,release_dates.platform,cover.cloudinary_id,game_modes"
+
 
     def self.search(query, fields = nil, filter = nil, limit = nil, offset = nil)
       url = "#{@@base_url}/#{@path}/?search=#{query}&fields="
-      fields.nil? ? url += @@defualt_fields : url += fields
+      url += fields || @defualt_fields
       url += "&filter#{filter}" if filter
       url += "&limit=#{limit}" if limit
       url += "&offset=#{offset}" if offset
       HTTParty.get(url, headers: @@headers)
     end
+
+    def self.all(fields = nil, filter = nil, order = nil, offset = 0, limit = 20)
+      url = "#{@@base_url}/#{@path}/?fields="
+      url += fields || @defualt_fields
+      url += "&filter#{filter}" if filter
+      url += "&order="
+      url += order || @order
+      url += "&limit=#{limit}"
+      url += "&offset=#{offset}"
+      HTTParty.get(url, headers: @@headers)
+    end
   end
 
-  class Game < Base
-    @path = 'games'
-    # def search_url(query)
-    #   "https://igdbcom-internet-game-database-v1.p.mashape.com/games?#{query}"
-    # end
-    # def self.search(query)
-    #   HTTParty.get(search_url(query), headers: headers).parsed_response
-    # end
+  class Game < IGDB::Base
+    @path = "games"
+    @defualt_fields = "name,release_dates.platform,cover.cloudinary_id,game_modes"
+    @order = "popularity:desc"
   end
 
-  class Platform < Base
-    @path = 'platforms'
+  class Platform < IGDB::Base
+    @path = "platforms"
+    @defualt_fields = "name"
+    @order = "generation:desc"
   end
 end
