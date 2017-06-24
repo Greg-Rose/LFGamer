9a416b31fe1f2c60019d16df535d5d77d5557b08
Greg Rose <gregrose628@gmail.com>
2017-06-24 00:55:24 -0400

    wip - seed_initial_games method

--- app/models/propagate_database.rb
+++ app/models/propagate_database.rb
@@ -1,9 +1,9 @@
 class PropagateDatabase
   def self.initial_seed
-    seed_starting_consoles
+    seed_initial_consoles
   end
 
-  private_class_method def self.seed_starting_consoles
+  private_class_method def self.seed_initial_consoles
     starting_consoles = [
       ["PlayStation 4", "PS4"],
       ["PlayStation 3", "PS3"],
@@ -17,6 +17,23 @@ class PropagateDatabase
         new_console.abbreviation = console[1] if console[1]
         new_console.save
       end
+    end
+  end
+
+  # private_class_method
+   def self.seed_initial_games
+    ps4_id = IGDB::Platform.search("PlayStation 4")[0]["id"]
+    filters = "[release_dates.platform][eq]=#{ps4_id}&filter[game_modes][eq]=2&filter[release_dates.date][lt]=2017-06-24&filter[category][eq]=0"
+    games = IGDB::Game.all(nil, filters, "popularity:desc", nil, 40)
+    multiplayer_id = 2
+    split_screen_id = 4
+
+    games.each do |game|
+      name = game["name"]
+      multiplayer = game["game_modes"].include?(multiplayer_id)
+      split_screen = game["game_modes"].include?(split_screen_id)
+      cover_image_url = "https://images.igdb.com/igdb/image/upload/t_cover_big/#{game['cover']['cloudinary_id']}.jpg"
+      
     end
   end
 end
