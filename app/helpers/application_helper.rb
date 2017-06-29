61ecb7058d47cc53d78e9c3bff3db132f18eebf2
Greg Rose <gregrose628@gmail.com>
2017-06-29 15:22:42 -0400

    made selected filter button color match console theme using added helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def add_console_color_to_button(console)
+    css_classes = ""
+    if current_page?(games_path(console: console.id))
+      if console.name.include?("PlayStation")
+        css_classes = " btn-playstation disabled"
+      end
+      if console.name.include?("Xbox")
+        css_classes = " btn-xbox disabled"
+      end
+    end
+    css_classes
+  end
 end
