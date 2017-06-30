5ac3db47bf26607211f5e44d08865e7249fccf82
Greg Rose <gregrose628@gmail.com>
2017-06-30 14:22:56 -0400

    added initial_seed to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Game.count == 0
+  PropagateDatabase.initial_seed
+end
