c9e61bf739ca7176be98b6a3a3c96e852a5833d5
Greg Rose <gregrose628@gmail.com>
2017-07-06 15:32:04 -0400

    added case insensitivity to username uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
-  validates :username, uniqueness: true
+  validates :username, uniqueness: { case_sensitive: false }
   validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "must only contain letters and numbers" }
   validates :admin, inclusion: { in: [true, false] }
 
