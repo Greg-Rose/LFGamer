d2e6ec7d30ab54567d2b273dc174023f24514bd2
Greg Rose <gregrose628@gmail.com>
2017-07-12 14:22:53 -0400

    added profile test factory

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :profile do
+    about_me "I like playing games. etc. etc. etc."
+    sequence(:psn_id) { |n| "Firefly#{n}" }
+    sequence(:xbox_gamertag) { |n| "FireFLY#{n}" }
+    zipcode "01234"
+    psn_id_public true
+    xbox_gamertag_public true
+    active true
+  end
+end
