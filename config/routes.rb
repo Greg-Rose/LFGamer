18d9f269b0d09e9ecb59cbf04ed1836f5cdad2ac
Greg Rose <gregrose628@gmail.com>
2017-07-12 16:56:50 -0400

    added profile edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get 'users/delete', action: 'delete', controller: 'registrations', as: 'delete_user_registration'
   end
   resources :games, only: [:index, :show]
+  resource :profile, only: [:edit, :update]
 
   root 'games#index'
 end
