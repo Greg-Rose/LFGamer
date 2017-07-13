492b47f8c8ad2c844b9894716506a65a3b2f2191
Greg Rose <gregrose628@gmail.com>
2017-07-13 16:02:46 -0400

    added update action to profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,29 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!, only: [:edit]
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def edit
     @profile = current_user.profile
+  end
+
+  def update
+    @profile = current_user.profile
+    new_profile = @profile.updated_at == @profile.created_at
+    if @profile.update(profile_params)
+      if new_profile
+        flash[:notice] = "Your profile has been created!"
+      else
+        flash[:notice] = "Your profile has been updated!"
+      end
+      redirect_to games_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:about_me, :psn_id, :xbox_gamertag,
+      :zipcode, :psn_id_public, :xbox_gamertag_public)
   end
 end
