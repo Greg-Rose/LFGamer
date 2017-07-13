320c9daa891206193c85159cecdd56faebb32cdc
Greg Rose <gregrose628@gmail.com>
2017-07-13 16:11:53 -0400

    added custom message to profiles zipcode validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :zipcode, format: { with: /\A\d{5}\z/ }, allow_nil: true
+  validates :zipcode, format: { with: /\A\d{5}\z/, message: "must be 5 digits" }, allow_nil: true
 end
