3a226f23b066a1a515f8b669d27675827d6d3af4
Greg Rose <gregrose628@gmail.com>
2017-07-13 16:09:25 -0400

    created form_error_messages! helper method and added it to profiles#edit view

---
+++ app/helpers/form_helper.rb
@@ -0,0 +1,19 @@
+module FormHelper
+  def form_error_messages!(record)
+    return '' if record.errors.empty?
+
+    messages = record.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+
+    html = <<-HTML
+    <div class="col-md-12">
+      <div class="alert alert-danger">
+        <ul>
+          #{messages}
+        </ul>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+end
