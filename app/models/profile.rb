72c3306a66bfcc541512f8ecfe68210dfeb8e934
Greg Rose <gregrose628@gmail.com>
2017-07-14 12:08:55 -0400

    added about me length validation to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,7 @@ class Profile < ApplicationRecord
   NULL_ATTRS = %w( psn_id xbox_gamertag about_me zipcode )
   before_save :nil_if_blank
 
+  validates :about_me, length: { maximum: 200 }
   validates :zipcode, format: { with: /\A\d{5}\z/, message: "must be 5 digits" }, allow_nil: true, allow_blank: true
 
   protected
