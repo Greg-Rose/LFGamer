feda3ac8f2848cb8fd521b8cc06510aad345ee45
Greg Rose <gregrose628@gmail.com>
2017-07-14 12:03:00 -0400

    fixed profile updates with blank fields

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,14 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :zipcode, format: { with: /\A\d{5}\z/, message: "must be 5 digits" }, allow_nil: true
+  NULL_ATTRS = %w( psn_id xbox_gamertag about_me zipcode )
+  before_save :nil_if_blank
+
+  validates :zipcode, format: { with: /\A\d{5}\z/, message: "must be 5 digits" }, allow_nil: true, allow_blank: true
+
+  protected
+
+  def nil_if_blank
+    NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
+  end
 end
