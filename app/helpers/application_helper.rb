73efb96341b585f1f0f748ed0d2777a44c8a30e5
Greg Rose <gregrose628@gmail.com>
2017-07-25 20:20:32 -0400

    changed table layout and styled lfgs list on games#show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,9 +16,9 @@ module ApplicationHelper
   def show_console_username_if_chosen(lfg)
     if lfg.show_console_username?
       if lfg.console.name.include?("PlayStation")
-        lfg.user.profile.psn_id
+        "- " + lfg.user.profile.psn_id if lfg.user.profile.psn_id?
       elsif lfg.console.name.include?("Xbox")
-        lfg.user.profile.xbox_gamertag
+        "- " + lfg.user.profile.xbox_gamertag if lfg.user.profile.xbox_gamertag?
       end
     end
   end
