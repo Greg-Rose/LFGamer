007a1285600b72178424128de5430fc75cea6d30
Greg Rose <gregrose628@gmail.com>
2017-07-25 22:39:03 -0400

    added test for editing LFG with too long specifics

--- spec/features/lfgs/03_user_edits_their_LFG_spec.rb
+++ spec/features/lfgs/03_user_edits_their_LFG_spec.rb
@@ -40,4 +40,15 @@ feature 'user edits their LFG for a game' do
     expect(Lfg.count).to eq 1
     expect(user.lfgs.count).to eq 1
   end
+
+  scenario 'specific is too long' do
+    sign_in user
+    visit game_path(game)
+    too_long_specifics = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis pi"
+    fill_in "Specifics", with: too_long_specifics
+    click_button "Look For Group"
+
+    expect(page).to have_content "Specifics is too long"
+    expect(page).to_not have_content too_long_specifics
+  end
 end
