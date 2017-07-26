72a1f7a36822d634a3a0b4f53e6a80e760248432
Greg Rose <gregrose628@gmail.com>
2017-07-26 11:41:06 -0400

    implemented auto updating fuzzy timestamps for LFGs list on games#show view with timeago jQuery plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
+//= require timeago
 //= require_tree .
+
+$(document).ready(function() {
+  $("time.timeago").timeago();
+});
