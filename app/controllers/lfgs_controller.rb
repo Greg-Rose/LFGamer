781959cafecd96a96613d26ae4f8cd78afc4aa46
Greg Rose <gregrose628@gmail.com>
2017-07-28 13:34:20 -0400

    refactored lfgs controller update action and added authorization check

--- app/controllers/lfgs_controller.rb
+++ app/controllers/lfgs_controller.rb
@@ -18,30 +18,27 @@ class LfgsController < ApplicationController
   def update
     @user = current_user
     @game = Game.find(params[:game][:id])
+    @lfg = Lfg.find(params[:id])
 
-    sql = "SELECT lfgs . * FROM lfgs INNER JOIN ownerships
-    ON lfgs . ownership_id = ownerships . id INNER JOIN games_consoles ON
-    ownerships . games_console_id = games_consoles . id WHERE ownerships . user_id = ? AND games_consoles .
-    game_id = ? LIMIT 1"
-    @lfg = (Lfg.find_by_sql [sql, @user, @game]).first
-
-    if @lfg.ownership_id != params[:lfg][:ownership_id].to_i
-      # If console is changed via ownership, delete lfg and create new so that
-      #  ActionCable properly removes LFG from previous console's LFG list
-      @lfg.destroy
-      @lfg = Lfg.new(lfg_params)
-      if @lfg.save
-        flash[:notice] = "Your LFG Has Been Updated!"
-        redirect_to @game
+    if @lfg.user == @user
+      if @lfg.ownership_id != params[:lfg][:ownership_id].to_i
+        # If console is changed via ownership, delete lfg and create new so that
+        #  ActionCable properly removes LFG from previous console's LFG list
+        @lfg.destroy
+        @lfg = Lfg.new(lfg_params)
+        if @lfg.save
+          flash[:notice] = "Your LFG Has Been Updated!"
+          redirect_to @game
+        else
+          render "games/show"
+        end
       else
-        render "games/show"
-      end
-    else
-      if @lfg.update_attributes(lfg_params)
-        flash[:notice] = "Your LFG Has Been Updated!"
-        redirect_to @game
-      else
-        render "games/show"
+        if @lfg.update_attributes(lfg_params)
+          flash[:notice] = "Your LFG Has Been Updated!"
+          redirect_to @game
+        else
+          render "games/show"
+        end
       end
     end
   end
