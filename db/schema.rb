7495873336937436434c3976a4d9d1ceacedeaee
Greg Rose <gregrose628@gmail.com>
2017-08-02 11:19:15 -0400

    added message migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802145316) do
+ActiveRecord::Schema.define(version: 20170802150944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,16 @@ ActiveRecord::Schema.define(version: 20170802145316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ownership_id"], name: "index_lfgs_on_ownership_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "ownerships", force: :cascade do |t|
