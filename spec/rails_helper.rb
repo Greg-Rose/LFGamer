0cb31cce6cc06a7713f05471b2f9b8eedf066d2f
Greg Rose <gregrose628@gmail.com>
2017-08-10 21:38:45 -0400

    removed background image url from capybara test blacklist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,7 @@ Capybara.javascript_driver = :poltergeist
 
 Capybara.register_driver :poltergeist do |app|
   #  Change domain name as necessary
-  options = { url_blacklist: ['/uploads/game/cover_image/*/test_cover.jpg', '/backgrounds/black_linen_V2.png'] } # you can also use * as a wildcard
+  options = { url_blacklist: ['/uploads/game/cover_image/*/test_cover.jpg'] } # you can also use * as a wildcard
   Capybara::Poltergeist::Driver.new(app, options)
 end
 
