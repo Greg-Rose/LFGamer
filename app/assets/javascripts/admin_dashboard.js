c66860e5c1e9f293fc12fcf01022168513a9458f
Greg Rose <gregrose628@gmail.com>
2017-08-14 18:46:10 -0400

    styled admin/users#index accounts counters, changed admin/dashboard#index count stats buttons on click with JS

--- app/assets/javascripts/admin_dashboard.js
+++ app/assets/javascripts/admin_dashboard.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".admin-dashboard").on("click", ".count-stats", function(event) {
     $( ".admin-dashboard h2").slideUp(500);
+    $(".admin-dashboard .count-stats").css("background-color", "#4d4d4d");
+    $(this).css("background-color", "#333333");
   });
 
   $(".admin-dashboard").on("click", ".users-count a", function(event) {
