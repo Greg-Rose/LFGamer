752144abe84994359d2681e95dd68ac2724ed105
Greg Rose <gregrose628@gmail.com>
2017-08-22 13:54:31 -0400

    added admin/consoles#index route

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
     resources :users, only: [:index, :show]
+    resources :consoles, only: [:index]
   end
 
   root 'games#index'
