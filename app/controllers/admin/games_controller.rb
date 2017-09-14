d99179ddcddd0b118a4b91abe770fcc1f9aa5ab0
Greg Rose <gregrose628@gmail.com>
2017-09-14 00:17:21 -0400

    filtered searched games results in admin/games#search so they only include games on valid consoles and no dlc, use this code until IGDB API fixes its [any] filter postfix for release_dates.platform

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -10,7 +10,23 @@ class Admin::GamesController < AdminController
 
   def search
     search = params["search"]
-    @searched_games = IGDB::Game.search(search, nil, ["[cover.cloudinary_id][exists]"])
+    @searched_games = IGDB::Game.search(search, nil, ["[cover.cloudinary_id][exists]", "[release_dates.platform][exists]", "[category][eq]=0"])
+    # remove/replace once IGDB API fixes multiple consoles filter with [any] for games
+    @searched_games.select! do |game|
+      valid_console = false
+      Console.all.each do |console|
+        api_id = IGDB::Platform.search(console.name).first["id"]
+        game["release_dates"].each do |rd|
+          if rd["platform"] == api_id
+            valid_console = true
+            break
+          end
+        end
+      end
+
+      next valid_console
+    end
+    # ^end remove after...^
     render layout: false
   end
 end
