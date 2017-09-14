6e7de8cd0dab5820919231d934a2d9a172749b9d
Greg Rose <gregrose628@gmail.com>
2017-09-14 01:07:24 -0400

    commented out code that made too many requests to IGDB API

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -12,20 +12,21 @@ class Admin::GamesController < AdminController
     search = params["search"]
     @searched_games = IGDB::Game.search(search, nil, ["[cover.cloudinary_id][exists]", "[release_dates.platform][exists]", "[category][eq]=0"])
     # remove/replace once IGDB API fixes multiple consoles filter with [any] for games
-    @searched_games.select! do |game|
-      valid_console = false
-      Console.all.each do |console|
-        api_id = IGDB::Platform.search(console.name).first["id"]
-        game["release_dates"].each do |rd|
-          if rd["platform"] == api_id
-            valid_console = true
-            break
-          end
-        end
-      end
-
-      next valid_console
-    end
+    # *** BROKEN - MAKES TOO MANY REQUESTS TO API ***
+    # @searched_games.select! do |game|
+    #   valid_console = false
+    #   Console.all.each do |console|
+    #     api_id = IGDB::Platform.search(console.name).first["id"]
+    #     game["release_dates"].each do |rd|
+    #       if rd["platform"] == api_id
+    #         valid_console = true
+    #         break
+    #       end
+    #     end
+    #   end
+    #
+    #   next valid_console
+    # end
     # ^end remove after...^
     render layout: false
   end
