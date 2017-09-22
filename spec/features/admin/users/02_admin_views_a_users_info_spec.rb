efdbaf7f0e77406377ca1b4481c6525483940596
Greg Rose <gregrose628@gmail.com>
2017-09-22 16:03:41 -0400

    fixed JS integration tests inconsistency/false failures

--- spec/features/admin/users/02_admin_views_a_users_info_spec.rb
+++ spec/features/admin/users/02_admin_views_a_users_info_spec.rb
@@ -66,6 +66,7 @@ feature 'admin views a users info' do
     visit admin_path
     find('.users-count').click
     find("a##{users[1].username}").click
+    sleep 1
     find('#userModal .close').click
 
     expect(page).to_not have_css "#userModal"
