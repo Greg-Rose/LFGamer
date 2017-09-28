2e1f8a803286fe05bbd012ab854d9738d17b010a
Greg Rose <gregrose628@gmail.com>
2017-09-28 02:46:32 -0400

    updated admin adds new console test to inlcude testing for games being checked for association with new console

--- spec/features/admin/consoles/02_admin_adds_new_console_spec.rb
+++ spec/features/admin/consoles/02_admin_adds_new_console_spec.rb
@@ -14,22 +14,26 @@ feature 'admin adds new console' do
   #   - Clicking it add's the console to the database and updates the current games if they're available for the new console
 
   let!(:admin) { create(:user, admin: true) }
+  let!(:game) { create(:game, name: "The Jackbox Party Pack 3", igdb_id: 19082) }
 
   scenario 'add console', js: true do
+    original_count = Console.count
     sign_in admin
     visit admin_path
     find('.consoles-count').click
     click_link 'add-console-btn'
-    fill_in 'add-console-search', with: "playstation 4"
+    fill_in 'add-console-search', with: "switch"
     VCR.use_cassette("admin/search-new-console") do
       find('.modal #search-btn').click
       sleep 1
-      fill_in 'add-console-abbreviation', with: "PS4"
+      fill_in 'add-console-abbreviation', with: "Switch"
       VCR.use_cassette("admin/add_new_console") do
         click_button 'add-console-submit-btn'
         sleep 1
-        expect(Console.count).to eq 1
-        expect(page).to have_content 'PlayStation 4 has been added'
+        expect(Console.count).to eq original_count + 1
+        expect(page).to have_content 'Nintendo Switch has been added'
+        expect(Console.last.name).to eq 'Nintendo Switch'
+        expect(Console.last.games.count).to eq 1
       end
     end
   end
