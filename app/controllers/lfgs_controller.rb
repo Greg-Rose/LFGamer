ce6e29c340d16b0055aa38ba511ea19c0ccaaaf3
Greg Rose <gregrose628@gmail.com>
2017-11-06 15:01:50 -0500

    added lfgs#index action

--- app/controllers/lfgs_controller.rb
+++ app/controllers/lfgs_controller.rb
@@ -1,6 +1,10 @@
 class LfgsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :create, :update, :destroy]
 
+  def index
+    @lfgs = current_user.lfgs
+  end
+  
   def create
     @user = current_user
     @game = Game.find(params[:game][:id])
